8afe7d0b26147e22368125d25608541c1961a117
augleih <augleih801@gmail.com>
2021-04-07 01:23:05 +0900

    날짜 정보 추가
    Motivation:
    todo의 기능에 맞게 오늘 날짜 정보 추가
    
    Modifications:
    clock.js
    날짜 상수 today 추가 및 데이터 반영 후 출력
    
    index.html
    h2 선언 (날짜)
    기존 js-clock 클래스(h1) h3 태그로 변경
    
    Result:
    날짜 정보 추가 및 기존 태그 변경

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,17 @@
 const clockContainer = document.querySelector(".js-clock"),
-clockTitle = clockContainer.querySelector(".js-title"); // ,를 통해 const 선언 시 선언 중복을 피할 수 있습니다.
+    dateInfo = clockContainer.querySelector(".js-date"),
+    clockTitle = clockContainer.querySelector(".js-title"); // ,를 통해 const 선언 시 선언 중복을 피할 수 있습니다.
 
 function getTime() {
     const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const today = `${date.getFullYear()}.${date.getMonth()}.${date.getDate()}`
+
+    const hours = date.getHours(),
+        minutes = date.getMinutes(),
+        seconds = date.getSeconds();
+
+    dateInfo.innerText = today;
+
     clockTitle.innerText = `${ 
         hours < 10 ? `0${hours}` : hours // hours에 대하여 10 보다 작으면 0을 붙여서 반환, 아니라면 그냥 반환
     }:${
