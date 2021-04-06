ff921d8e828ef30e3696db073a8c5ebd728b6cfc
augleih <augleih801@gmail.com>
2021-04-06 21:44:49 +0900

    배경화면 설정
    Motivation
    크롬 시작 페이지와 같이 다양한 배경화면 임의로 출력되도록 구현
    
    Modification
    background.js
    
    Modified
    배경화면 출력 기능 구현

---
+++ js/background.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 10;
+
+function printImage(imgNumber) { // 실제 이미지를 출력하는 함수
+    const image = new Image();
+    image.src = `./images/${imgNumber}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function generateRandom() { // 랜덤 숫자를 생성하는 함수
+    const number = Math.ceil(Math.random() * IMG_NUMBER); // 올림, Math.floor() -- 내림
+    return number;
+}
+
+function init () { // 페이지 호출 시 실행될 함수들을 호출하는 함수
+    const randomNum = generateRandom();
+    printImage(randomNum);
+}
+
+init();
